7e56b158ed0eee021adfbcec9e3a9d1fb1ae7d28
mass10 <mass10.github@gmail.com>
2017-02-05 17:44:05 +0900

    wip

--- code/date-and-time/creating-date-time.php
+++ code/date-and-time/creating-date-time.php
@@ -4,19 +4,35 @@
 function _main() {
 
 	{
-		$date = date_create("2017-02-03");
-		printf("[%s]\n", $date->format("Y-m-d"));
-		printf("[%s]\n", $date->format("Y-m-d H:i:s"));
-	}
-
-	{
+		printf("--- %s ---\n", __LINE__);
 		$date = new DateTime();
+		printf("[%s]\n", $date->getTimestamp());
 		printf("[%s]\n", $date->format("Y-m-d"));
 		printf("[%s]\n", $date->format("Y-m-d H:i:s"));
 	}
 
 	{
+		printf("--- %s ---\n", __LINE__);
+		$date = date_create("2017-02-03");
+		printf("[%s]\n", $date->getTimestamp());
+		printf("[%s]\n", $date->format("Y-m-d"));
+		printf("[%s]\n", $date->format("Y-m-d H:i:s"));
+	}
+
+	{
+		printf("--- %s ---\n", __LINE__);
+		$date = new DateTime();
+		$date->setDate(2000, 2, 10);
+		$date->setTime(0, 0, 0);
+		printf("[%s]\n", $date->getTimestamp());
+		printf("[%s]\n", $date->format("Y-m-d"));
+		printf("[%s]\n", $date->format("Y-m-d H:i:s"));
+	}
+
+	{
+		printf("--- %s ---\n", __LINE__);
 		$date = new DateTime("2010-01-01 12:25:59.000");
+		printf("[%s]\n", $date->getTimestamp());
 		printf("[%s]\n", $date->format("Y-m-d"));
 		printf("[%s]\n", $date->format("Y-m-d H:i:s"));
 	}
